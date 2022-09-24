731c822a534a650d931be030eba18daf303731c6
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2022-09-24 16:24:26 -0400

    Update interface with new methods

--- store/store.go
+++ store/store.go
@@ -1,11 +1,16 @@
 package store
 
-import "github.com/peschkaj/togo"
+import (
+	"github.com/peschkaj/togo"
+	"time"
+)
 
 type Store interface {
 	AddOrUpdateTask(togo.Task)
 	RemoveTask(togo.Task) bool
 	FindTaskByName(string) (*togo.Task, bool)
+	FindByDueDate(*time.Time) []togo.Task
+	OverdueTasks() []togo.Task
 	Count() int
 	All() []togo.Task
 }
