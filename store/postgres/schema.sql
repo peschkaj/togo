ff5c77d7a5e6963ec4def49033f1944a14e638c7
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2022-10-02 09:26:34 -0400

    Update schema to include projects

--- store/postgres/schema.sql
+++ store/postgres/schema.sql
@@ -5,11 +5,18 @@ CREATE ROLE togo_user
 
 CREATE SCHEMA IF NOT EXISTS togo;
 
-CREATE TABLE IF NOT EXISTS togo.tasks (
+CREATE TABLE IF NOT EXISTS togo.projects (
     id BIGSERIAL PRIMARY KEY ,
     name VARCHAR(100) NOT NULL,
+    description VARCHAR NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS togo.tasks (
+    id BIGSERIAL PRIMARY KEY ,
+    project_id BIGINT NOT NULL REFERENCES togo.projects(id),
+    name VARCHAR(100) NOT NULL,
     description VARCHAR NOT NULL,
-    priority INT NOT NULL,
+    priority INT NOT NULL DEFAULT 0,
     created_on TIMESTAMPTZ(6) NOT NULL,
     completed_on TIMESTAMPTZ(6) NULL,
     due_date TIMESTAMPTZ(6) NULL
@@ -17,6 +24,7 @@ CREATE TABLE IF NOT EXISTS togo.tasks (
 
 CREATE UNIQUE INDEX ux_tasks_name ON togo.tasks(name);
 CREATE INDEX ix_tasks_due_date ON togo.tasks(due_date);
+CREATE INDEX ix_tasks_project ON togo.tasks(project_id);
 
 GRANT USAGE ON SCHEMA togo TO togo_user;
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA togo TO togo_user;
